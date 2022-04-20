1bed8179de06d04c430696971596c28a924247bb
elinamakarenko <68436291+elinamakarenko@users.noreply.github.com>
2022-04-20 14:48:30 +0300

    first-comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,22 @@
+import Profile from "./Profile";
+import Statistics from "./Statistics";
+import user from "./user.json";
+import data from "./data.json";
+import friends from "./friends.json";
+import transactions from "./transactions.json";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <div>
+        <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+   <Statistics title="Upload stats" stats={data} />
+   <Statistics stats={data} />
+   </div>
   );
 };
